839e326a2804d880c013352261fbc165aec0f7bb
vrishbhanusingh <vrishbhanu28@gmail.com>
2024-05-29 19:41:45 +0530

    database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.POSTGRES_URL!,
   },
   tablesFilter: ["nextjs-tut-t3_*"],
 } satisfies Config;
